09dc3cdf45f134afd80982016946ad354af05b9f
Yaroslav Isaienkov <YIsaienkov@aug.global>
2020-08-03 16:22:30 +0300

    Testing

--- evaluations/classification/basic/accuracy.py
+++ evaluations/classification/basic/accuracy.py
@@ -6,6 +6,20 @@ __all__ = [
 
 
 def accuracy(y_true: List, y_pred: List) -> float:
+    """
+
+    Parameters
+    ----------
+    y_true : list
+        True labels
+    y_pred : list
+        Predicted labels
+
+    Returns
+    -------
+    float
+        Accuracy score
+    """
     count_true = sum([i == j for i, j in zip(y_true, y_pred)])
 
     count_total = len(y_true)
