f5edf428f022c8580336b7d881408416bf34dc39
Yaroslav Isaienkov <YIsaienkov@aug.global>
2020-08-03 14:44:33 +0300

    Testing

--- evaluations/classification/basic/accuracy.py
+++ evaluations/classification/basic/accuracy.py
@@ -5,7 +5,7 @@ __all__ = [
 ]
 
 
-def accuracy(y_true: List, y_pred: List):
+def accuracy(y_true: List, y_pred: List) -> float:
     count_true = sum([i == j for i, j in zip(y_true, y_pred)])
 
     count_total = len(y_true)
