d855006961f985cdd749c0f1545d86b5c0b24d13
Yaroslav Isaienkov <YIsaienkov@aug.global>
2020-08-03 14:02:57 +0300

    Add typing to accuracy function

--- evaluations/classification/basic/accuracy.py
+++ evaluations/classification/basic/accuracy.py
@@ -1,9 +1,11 @@
+from typing import List
+
 __all__ = [
     'accuracy'
 ]
 
 
-def accuracy(y_true, y_pred):
+def accuracy(y_true: List, y_pred: List) -> float:
     count_true = sum([i == j for i, j in zip(y_true, y_pred)])
 
     count_total = len(y_true)
