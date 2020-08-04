b8207bfa36355d7c62608c905b01bbaee30735e1
Yaroslav Isaienkov <YIsaienkov@aug.global>
2020-08-04 11:58:39 +0300

    Testing

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -1,13 +1,24 @@
+"""Tests for accuracy function"""
+
 from evaluations.classification.basic import accuracy
 
 
-def test_all_true():
+def test_accuracy_all_true():
+    """
+    All predictions true
+    """
     assert accuracy([1, 1, 0, 0], [1, 1, 0, 0]) == 1.
 
 
-def test_half_true():
+def test_accuracy_half_true():
+    """
+    50% predictions true
+    """
     assert accuracy([1, 1, 0, 0], [1, 0, 1, 0]) == .5
 
 
-def test_no_true():
+def test_accuracy_no_true():
+    """
+    No true predictions
+    """
     assert accuracy([1, 1, 0, 0], [0, 0, 1, 1]) == 0.
